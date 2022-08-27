daebc4aea3ad34ecb4d0327b8822e50263f0d62c
Süleyman Kaplan <68708734+suleykaps@users.noreply.github.com>
2022-08-28 00:32:16 +0300

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -1,7 +1,12 @@
+# libraries
+
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn import datasets
 
+
+st.title("StarLordDataPage")
 st.header("**Star Lord'un Uzay Verileri - Star Lord's Space Data's**")
 st.markdown("Ben **Süleyman Kaplan**. **Star Lord** olarak kendimi veri biliminde geliştirmekteyim. "
             "**Uzay ve Uydu Mühendisliği** 1. sınıf öğrencisi olarak eğitim hayatıma devam etmekteyim. **ABD Ankara"
@@ -12,19 +17,27 @@ st.markdown("Ben **Süleyman Kaplan**. **Star Lord** olarak kendimi veri bilimin
             "**CarryVibe Logistics Technologies** adlı girişimimde iş geliştirme, tasarım, planlama ve bağlantı oluşturma konularında kurucu ortak ve takım lideri görevlerini yürütmekteyim. "
             "Şu anda **veri bilimi, makine öğrenmesi** ve **MBA** alanlarında kendimi geliştirmekteyim.")
 
-
-
-
-st.subheader("SpaceX Görevleri (2006-Şimdi)")
+# datasets
 spacex_data = pd.read_csv("spacex_missions.csv")
-st.dataframe(spacex_data)
+airport_data = pd.read_csv("airport_desc.csv", encoding='latin1')
 
 
-st.subheader("100 Adet Havalimanının Uzaydan Görüntüleri")
-airport_data = pd.read_csv("airport_desc.csv", encoding = 'latin1')
-st.dataframe(airport_data)
+#page settings
 
+dataSet_name = st.sidebar.selectbox("Uzay Veri Setlerinden Birini Seç",("SpaceX Görevleri","100 HavaLimanı" ,"100 HavaLimanı Uzay Görüntüsü" ))
 
+if dataSet_name == "SpaceX Görevleri":
+    st.subheader("**SpaceX Görevleri (2006-Şimdi)**")
+    st.dataframe(spacex_data)
 
+elif dataSet_name == "100 HavaLimanı" :
+    st.subheader("**100 Adet Havalimanının Uzaydan Görüntüleri**")
+    st.dataframe(airport_data)
 
-st.markdown("powered by @StarLord")
+elif dataSet_name == "100 HavaLimanı Uzay Görüntüsü":
+    st.subheader("Seçilen Havalimanının Uzaydan Görüntüsü")
+    number = int(st.number_input('\n\n\n\n**1-100 Arasında Sayı Seçerek Havalimanı Görüntüsüne Ulaşabilirsiniz**'))
+
+    st.write("Girilen numara: ", number)
+    airport_data.iloc[number - 1:number]
+
